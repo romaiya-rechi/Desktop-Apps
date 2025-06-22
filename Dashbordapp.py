6de9359d49db7119d6ba3e57a903b749a898f057
Romaiya Sharmin Rechi <150883564+romaiya-rechi@users.noreply.github.com>
2025-06-22 16:13:16 +0600

    Add files via upload

---
+++ Dashbordapp.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl
+
+class WebApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("TemplateEdge App")
+        self.setGeometry(100, 100, 1280, 800)
+
+        browser = QWebEngineView()
+        browser.load(QUrl("https://templateedge.com/wp-admin"))
+        self.setCentralWidget(browser)
+
+app = QApplication(sys.argv)
+window = WebApp()
+window.show()
+sys.exit(app.exec_())
